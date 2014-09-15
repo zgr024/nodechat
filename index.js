faf81782094cade12a6ac68a4a1c7b52ce8deef0
Zachary Rosenberg <zgr024@gmail.com>
2014-09-15 17:17:30 -0400

    js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, '../application', 'index.html'));
 });
 
 http.listen(3000, function(){
